87e6fcbc21b6bb0437e9a562e359fb577eee6193
Jean-Baptiste Queru <jbqueru@gmail.com>
2024-10-15 12:03:34 +0300

    Minimum prototype of menu sprite

--- menu.s
+++ menu.s
@@ -45,6 +45,74 @@ Menu:
 	cmp.w	(a0), d0
 	beq.s	.WaitVbl
 
+	not.w	$ffff8240.w
+
+	movea.l	gfx_os_fb, a0
+	lea	40+40*160(a0), a0
+
+	move.w	#2, BLIT_SRC_XINC
+	move.w	#114, BLIT_SRC_YINC
+
+	move.w	#$ffff, BLIT_ENDMASK1
+	move.w	#$ffff, BLIT_ENDMASK2
+	move.w	#$ffff, BLIT_ENDMASK3
+
+	move.w	#2, BLIT_DST_XINC
+	move.w	#114, BLIT_DST_YINC
+
+	move.w	#24, BLIT_XCOUNT
+
+	move.w	#(BLIT_HOP_SRC << 8) + BLIT_OP_ST + BLIT_OP_SNT, BLIT_HOPOP
+
+	move.b	#BLIT_SHIFT_NISR + BLIT_SHIFT_IFSR, BLIT_SHIFT
+	move.l	#menu_background + 40 + 40 * 160, BLIT_SRC_ADDR
+	move.l	a0, BLIT_DST_ADDR
+	move.w	#80, BLIT_YCOUNT
+	move.b	#BLIT_CTRL_HOG + BLIT_CTRL_BUSY, BLIT_CTRL
+
+	nop
+	nop
+
+
+	move.w	#2, BLIT_SRC_XINC
+	move.w	#2, BLIT_SRC_YINC
+
+	move.w	#$ffff, BLIT_ENDMASK1
+	move.w	#$ffff, BLIT_ENDMASK2
+	move.w	#$0000, BLIT_ENDMASK3
+
+	move.w	#8, BLIT_DST_XINC
+	move.w	#120, BLIT_DST_YINC
+
+	move.w	#6, BLIT_XCOUNT
+
+	move.w	#(BLIT_HOP_SRC << 8) + BLIT_OP_NST, BLIT_HOPOP
+
+	move.b	#BLIT_SHIFT_NISR + BLIT_SHIFT_NFSR, BLIT_SHIFT
+
+	moveq.l	#3, d7
+.ErasePlane:
+	move.l	#menu_sprite, BLIT_SRC_ADDR
+	move.l	a0, BLIT_DST_ADDR
+	move.w	#80, BLIT_YCOUNT
+	move.b	#BLIT_CTRL_HOG + BLIT_CTRL_BUSY, BLIT_CTRL
+	addq.l	#2, a0
+	dbra.w	d7, .ErasePlane
+
+	subq.l	#8, a0
+	move.w	#(BLIT_HOP_SRC << 8) + BLIT_OP_ST + BLIT_OP_SNT + BLIT_OP_NST, BLIT_HOPOP
+
+	moveq.l	#3, d7
+.DrawPlane:
+	move.l	#menu_sprite, BLIT_SRC_ADDR
+	move.l	a0, BLIT_DST_ADDR
+	move.w	#80, BLIT_YCOUNT
+	move.b	#BLIT_CTRL_HOG + BLIT_CTRL_BUSY, BLIT_CTRL
+	addq.l	#2, a0
+	dbra.w	d7, .DrawPlane
+
+	not.w	$ffff8240.w
+
 	cmpi.b	#$39, $fffffc02.w
 	bne.s	.UpDone
 	move.b	#1, menu_space_pressed.l
@@ -62,6 +130,17 @@ menu_background:
 	.incbin		"out/inc/menu_bitmap.bin"
 menu_palette:
 	.incbin		"out/inc/menu_palette.bin"
+
+menu_sprite:
+	.rept	8
+	.dc.w	$ffff, $ffff, $ffff, $ffff, $ffff
+	.endr
+	.rept	64
+	.dc.w	$ff00, $0000, $0000, $0000, $00ff
+	.endr
+	.rept	8
+	.dc.w	$ffff, $ffff, $ffff, $ffff, $ffff
+	.endr
 
 	.bss
 menu_space_pressed:
